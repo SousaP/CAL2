5e2e34c36ff15db3894763ae11bc24c3a36c0f2e
Vítor Teixeira <vitor_filipe_@hotmail.com>
2014-06-02 04:52:39 +0100

    fds não me deixa fazer sync

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,7 @@ void CompareFiles(Files F1, Files F2) {
 	F.CreatCompareFile();
 	F.showDiff();
 	cout << "\n\nFA -> Ficheiro antigo\nFN -> Ficheiro novo\nGuardado no ficheiro Compare.txt\n";
+
 }
 
 void MenuFiles() {
